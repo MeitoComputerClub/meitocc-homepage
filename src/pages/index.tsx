575d8cd24833b846ed717bc2850704c920b44b92
7fa40e <118368827+7fa40e@users.noreply.github.com>
2026-03-22 11:15:37 +0900

    サイトトップ メニュー画像

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,62 @@
+import styles from "./index.module.css";
+
 export default function Home() {
   return (
-    <>
-      <div className="top">
-        <h1>Meito</h1>
-        <h1>Computer</h1>
-        <h1>Club</h1>
-      </div>
+    <main className={styles.container}>
+      <section className={styles.hero}>
+        <div className={styles.heroLeft}>
+          <h1 className={styles.title}>
+            M~~~~
+            <br />
+            COMPUTER
+            <br />
+            CLUB
+          </h1>
+          <p className={styles.catch}>design and development</p>
+        </div>
+      </section>
 
-      <div className="title">
-        <h2>About us</h2>
-      </div>
+      <section className={styles.info}>
+        <h2 className={styles.aboutTitle}>About us</h2>
 
-      <div className="details">
-        <h3>活動内容</h3>
-        <ul>
-          <li>動画編集</li>
-          <li>Pythonを用いたゲーム開発</li>
-          <li>Webサイト、Webアプリの開発</li>
-          <li>イラスト制作</li>
-        </ul>
-      </div>
+        <div className={styles.cards}>
+          <div className={styles.card}>
+            <h3>活動内容</h3>
+            <p>
+              動画編集
+              <br />
+              Pythonを用いたゲーム開発
+              <br />
+              Webサイト、Webアプリの開発
+              <br />
+              イラスト制作
+            </p>
+          </div>
 
-      <div className="time">
-        <h3>活動場所・時間</h3>
-        <p>木曜日を除いた平日の授業後、15:40から17:00まで</p>
-        <p>北校舎3階の小ゼミナール室で活動しています。</p>
-      </div>
+          <div className={styles.card}>
+            <h3>活動場所・時間</h3>
+            <p>
+              木曜日を除いた平日の授業後
+              <br />
+              15:40から17:00まで
+              <br />
+              北校舎3階の小ゼミナール室で活動しています。
+            </p>
+          </div>
 
-      <div className="people">
-        <h3>活動人数</h3>
-        <ul>
-          <li>1年生: 11人</li>
-          <li>2年生: 11人</li>
-        </ul>
-        <p>※2025年度時点</p>
-      </div>
-    </>
+          <div className={styles.card}>
+            <h3>活動人数</h3>
+
+            <p>
+              1年生: 11人
+              <br />
+              2年生: 11人
+            </p>
+
+            <p>※2025年度時点</p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
