3919c7d338a0aa6d95718f04e87dd8db52cc7e27
7fa40e <118368827+7fa40e@users.noreply.github.com>
2026-05-08 00:41:26 +0900

    meta dataの修正 <Head>内に記述

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,20 @@ import type { AppProps } from "next/app";
 import { sora, noto, zen } from "@/lib/fonts";
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
+import Head from "next/head";
 
-export const metadata = {
-  title: "MeitoCC",
-  description: "名東高校コンピュータ部",
-};
+// export const metadata = {
+//   title: "MeitoCC",
+//   description: "名東高校コンピュータ部",
+// };
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={zen.className}>
+      <Head>
+        <title>MeitoCC</title>
+        <meta name="description" content="名東高校コンピュータ部"></meta>
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
