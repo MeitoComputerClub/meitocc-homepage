2dc38752d157a8d9bd06d4f8cf79af8926e6f187
7fa40e <118368827+7fa40e@users.noreply.github.com>
2026-09-06 21:20:11 +0900

    各ページにmeta descriptionを追加した。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className={zen.className}>
       <Head>
         <title>MeitoCC</title>
-        <meta name="description" content="名東高校コンピュータ部"></meta>
+        <meta
+          name="description"
+          content="名東高校コンピュータ部の公式サイトです。"
+        ></meta>
         <meta
           name="google-site-verification"
           content="i4SLLwBEHfq8G249jKdCxgEdzQkPFgTw-h6_ZJNAOAs"
--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -1,15 +1,21 @@
+import Head from "next/head";
 export default function Question() {
   return (
-    <section className="relative z-0 min-h-screen overflow-hidden bg-[#111] p-10 text-white">
-      {/* 背景グリッド線 */}
-      <div className="pointer-events-none absolute inset-0 -z-10 bg-[linear-gradient(rgba(255,255,255,0.03)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.09)_1px,transparent_1px)] mb-2.5 bg-size-[40px_40px]" />
-      <h1 className="text-[clamp(1.5rem,3vw,2.5rem)] text-center underline underline-offset-10 decoration-2 decoration-[#ff8000]">
-        Question
-      </h1>
-      <p className="text-[clamp(0.77rem,3vw,1rem)] opacity-80 text-center mt-1.5">
-        {/* よくある質問 */}
-        Coming Soon...
-      </p>
-    </section>
+    <>
+      <Head>
+        <meta name="description" content="弊部に寄せられた質問に答えます" />
+      </Head>
+      <section className="relative z-0 min-h-screen overflow-hidden bg-[#111] p-10 text-white">
+        {/* 背景グリッド線 */}
+        <div className="pointer-events-none absolute inset-0 -z-10 bg-[linear-gradient(rgba(255,255,255,0.03)_1px,transparent_1px),linear-gradient(90deg,rgba(255,255,255,0.09)_1px,transparent_1px)] mb-2.5 bg-size-[40px_40px]" />
+        <h1 className="text-[clamp(1.5rem,3vw,2.5rem)] text-center underline underline-offset-10 decoration-2 decoration-[#ff8000]">
+          Question
+        </h1>
+        <p className="text-[clamp(0.77rem,3vw,1rem)] opacity-80 text-center mt-1.5">
+          {/* よくある質問 */}
+          Coming Soon...
+        </p>
+      </section>
+    </>
   );
 }
